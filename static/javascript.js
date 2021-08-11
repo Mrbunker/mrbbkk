bd53d94de2215664842153ea50e3b82b9fcdec58
mrbunker <Mrbunker>
2021-08-11 21:05:09 +0800

    audio

--- static/javascript.js
+++ static/javascript.js
@@ -1,39 +1,68 @@
-const languageSwitchBtn = document.querySelector('.btn-item-language');
+function languageEvent() {
+  let domTitle = document.querySelector('div.title[language]');
+  let domBlockquote = document.querySelector('blockquote[language]');
+  let domBtn = document.querySelector('li.btn[language]');
+  // let domMain = document.querySelector('main');
 
-let languageList = {
-  en: {
-    title: 'this guy is <a href="#">mrbbkk</a>',
-    blockquote: '"You will never have what you like until you learn to like what you have."',
-    btn: 'switch language',
-  },
-  zh: {
-    title: '这个B叫作 <a href="#">mrbbkk</a>',
-    blockquote: '"欲得到你喜欢的东西，应先学会喜欢你已有的东西。"',
-    btn: '切换语言',
-  },
-};
+  function languageSwitch() {
+    let languageList = {
+      en: {
+        title: 'this guy is <span>mrbbkk</span>',
+        blockquote: '"You will never have what you like until you learn to like what you have."',
+        btn: 'switch language',
+      },
+      zh: {
+        title: '这个B叫作 <a href="">mrbbkk</a>',
+        blockquote: '"欲得到你喜欢的东西，应先学会喜欢你已有的东西。"',
+        btn: '切换语言',
+      },
+    };
 
-let domTitle = document.querySelector('h1[language]');
-let domBlockquote = document.querySelector('blockquote[language]');
-let domBtn = document.querySelector('li.btn[language]');
-// let domMain = document.querySelector('main');
-
-function languageSwitch() {
-  let currentLanguage = languageSwitchBtn.attributes.language.value;
-  console.log('switch', currentLanguage);
-  if (currentLanguage === 'en') {
-    languageSwitchBtn.attributes.language.value = 'zh';
-    document.body.style.fontFamily = 'system-ui';
-    domTitle.innerHTML = '什么毛病';
-    domBlockquote.innerHTML = languageList.zh.blockquote;
-    domBtn.innerHTML = languageList.zh.btn;
-  } else if (currentLanguage === 'zh') {
-    document.body.style.fontFamily = 'Covered,pingfang-sc，micsoft-yahei';
-    languageSwitchBtn.attributes.language.value = 'en';
-    domTitle.innerHTML = languageList.en.title;
-    domBlockquote.innerHTML = languageList.en.blockquote;
-    domBtn.innerHTML = languageList.en.btn;
+    let currentLanguage = languageSwitchBtn.attributes.language.value;
+    console.log('switch', currentLanguage);
+    if (currentLanguage === 'en') {
+      document.body.style.fontFamily = 'system-ui';
+      domTitle.innerHTML = '喂！三点几咧！';
+      domBlockquote.innerHTML = languageList.zh.blockquote;
+      domBtn.innerHTML = '做……做撚啊做！';
+      return (languageSwitchBtn.attributes.language.value = 'zh');
+    } else if (currentLanguage === 'zh') {
+      document.body.style.fontFamily = 'Covered,pingfang-sc，micsoft-yahei';
+      domTitle.innerHTML = languageList.en.title;
+      domBlockquote.innerHTML = languageList.en.blockquote;
+      domBtn.innerHTML = languageList.en.btn;
+      return (languageSwitchBtn.attributes.language.value = 'en');
+    }
   }
+  const languageSwitchBtn = document.querySelector('.btn-item-language');
+  languageSwitchBtn.addEventListener('click', languageSwitch);
 }
 
-languageSwitchBtn.addEventListener('click', languageSwitch);
+function audioEvent() {
+  let audio = document.querySelector('audio');
+  audio.play();
+  audio.volume = 0.015;
+  console.log(audio.volume);
+
+  mrbbkk.addEventListener('click', () => {
+    audio.volume = 0.1;
+    audio.play();
+  });
+
+  audio.addEventListener('timeupdate', function firstPause() {
+    if (audio.currentTime >= 1) {
+      audio.volume = 0.2;
+      audio.pause();
+      audio.removeEventListener('timeupdate', firstPause);
+    }
+  });
+  audio.addEventListener('timeupdate', function firstPause() {
+    if (audio.currentTime >= 4) {
+      audio.pause();
+      audio.removeEventListener('timeupdate', firstPause);
+    }
+  });
+}
+
+languageEvent();
+audioEvent();
